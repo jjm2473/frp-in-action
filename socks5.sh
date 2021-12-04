baa2e8d5b99475c31733c34510193ef735a37ee2
练亮斌 <1129525450@qq.com>
2021-12-04 23:46:34 +0800

    Update socks5.sh

--- socks5.sh
+++ socks5.sh
@@ -9,16 +9,23 @@ readu1() {
     readbytes $1 | hexdump -ve '1/1 " %d"' | dd bs=1 skip=1 2>/dev/null
 }
 
+skipver() {
+    local ver=`readu1 1`
+    if [ $ver -ne 5 ]; then
+        echo "Unsupported Version $VER" >&2
+        exit 1
+    fi
+}
 
 ## handshake req
 # read version, always 0x05
-readbytes 1 >/dev/null
+skipver
 
 # client supported METHODS_COUNT
 NM=`readu1 1`
 
 # read all client supported METHODS and ignore
-readbytes $NM >/dev/null
+[ $NM -gt 0 ] && readbytes $NM >/dev/null
 
 ## handshake resp
 # tell client we only support SOCKS5 (0x05) None auth (0x00)
@@ -26,8 +33,14 @@ echo -ne '\05\00'
 
 ## command req
 # skip VERSION, get COMMAND, skip RSV
-readbytes 1 >/dev/null
+skipver
 COMMAND=`readu1 1`
+if [ $COMMAND -ne 1 -a $COMMAND -ne 3 ]; then
+    echo "Unknown COMMAND $COMMAND" >&2
+    # not supportd command
+    echo -ne '\05\07\00\01\00\00\00\00\00\00'
+    exit 1
+fi
 readbytes 1 >/dev/null
 
 # get ADDRESS_TYPE
@@ -39,19 +52,18 @@ if [ $AT == 3 ]; then
     HL=`readu1 1`
     # read host
     ADDR=`readbytes $HL`
-else
-    # else ip address
+elif [ $AT == 1 ]; then
+    # else ipv4 address
     ADDR=`readu1 4 | sed 's/ /./g'`
+else
+    echo "Unsupported ADDRESS_TYPE $AT" >&2
+    echo -ne '\05\04\00\01\00\00\00\00\00\00'
+    exit 1
 fi
 # read port
 PORT=$((`readu1 1` * 256 + `readu1 1`))
 
 ## command resp
-if [ $COMMAND -ne 1 -a $COMMAND -ne 3 ]; then
-    # not supportd command
-    echo -ne '\05\07\00\01\00\00\00\00\00\00'
-    exit 1
-fi
 # response
 echo -ne '\05\00\00\01\00\00\00\00\00\00'
 
