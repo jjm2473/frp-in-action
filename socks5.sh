8be3e812a41bd069d6626c7e7031a7fbe2c9f2f2
练亮斌 <1129525450@qq.com>
2021-12-04 18:35:34 +0800

    Update socks5.sh

--- socks5.sh
+++ socks5.sh
@@ -9,7 +9,9 @@ readu1() {
     readbytes $1 | hexdump -e '1/1 " %d"' | dd bs=1 skip=1 2>/dev/null
 }
 
-# version, always 0x05
+
+## handshake req
+# read version, always 0x05
 readbytes 1 >/dev/null
 
 # client supported METHODS_COUNT
@@ -18,11 +20,15 @@ NM=`readu1 1`
 # read all client supported METHODS and ignore
 readbytes $NM >/dev/null
 
+## handshake resp
 # tell client we only support SOCKS5 (0x05) None auth (0x00)
 echo -ne '\05\00'
 
-# skip VERSION, COMMAND, RSV
-readbytes 3 >/dev/null
+## command req
+# skip VERSION, get COMMAND, skip RSV
+readbytes 1 >/dev/null
+COMMAND=`readu1 1`
+readbytes 1 >/dev/null
 
 # get ADDRESS_TYPE
 AT=`readu1 1`
@@ -39,7 +45,20 @@ else
 fi
 # read port
 PORT=$((`readu1 1` * 256 + `readu1 1`))
+
+## command resp
+if [ $COMMAND -ne 1 -a $COMMAND -ne 3 ]; then
+    # not supportd command
+    echo -ne '\05\07\00\01\00\00\00\00\00\00'
+    exit 1
+fi
 # response
 echo -ne '\05\00\00\01\00\00\00\00\00\00'
+
+PROTO="TCP"
+[ $COMMAND -eq 3 ] && PROTO="UDP"
+
 # nc $ADDR $PORT
-socat - TCP:$ADDR:$PORT
+TARGET=$PROTO:$ADDR:$PORT
+[ -n "$1" ] && TARGET=$TARGET,bindtodevice=$1
+exec socat - $TARGET
